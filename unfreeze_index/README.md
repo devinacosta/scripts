62aab33aae41e4eaba54c3b84f6656ca127a0b50
devin <devin@devins-MacBook-Pro.local>
2024-04-25 14:43:55 -0700

    Updated to 1.1.1

--- unfreeze_index/README.md
+++ unfreeze_index/README.md
@@ -1,9 +1,14 @@
 # unfreeze_index.py
 
-This utility unfreezes Elasitcsearch indices that have been Frozen by the ILM process. 
-The utility expects that you pass the following parameters:
 
-```
+I have developed a Python script that interfaces with Elasticsearch to manage index patterns. The script focuses on identifying and unfreezing index patterns that are currently in a frozen state. Using the Elasticsearch Python module, the script first connects to the Elasticsearch instance, taking into account the specified host, port, and SSL configuration. It then searches for frozen index patterns, leveraging Elasticsearch's API to query for indices with the "frozen" state. Upon finding these patterns, the script unfreezes them, allowing for the resumption of normal operations. The script is designed to be efficient and reliable, ensuring that frozen index patterns are promptly identified and addressed to maintain the Elasticsearch cluster's performance and availability.
+
+The screen expects 3 Parameters to be passed:
+ - Location (**-l**), it supports multiple locations being passed (i.e.: **esc01,iad41,iad01**)
+ - Date (**-d**), date needs to be in format **YYYY.mm.dd**
+ - Component (**-c**), it supports multiple components (i.e.: **pwr,gas,pas**)
+
+```commandline
 usage: unfreeze_index.py [-h] -l LOCATIONS -d DATE -c COMPONENT
 
 options:
@@ -12,36 +17,70 @@ options:
   -d DATE, --date DATE
   -c COMPONENT, --component COMPONENT
   ```
- You would typically pass logs-pas, logs-pwr, etc.
+ >You would typically pass logs-pas, logs-pwr, etc.
+ >The _*Date format*_ Needs to be in **YYYY.mm.dd**
  
-  ```
-  Example usage:
-   unfreeze_index.py -d 2023.04.25 -c 'logs-pas-main-stat' -l 'es1,es2'                                                                                                                                                                             
+ 
+  ```commandline 
+└─[$] ./unfreeze_index.py -d 2024.03.26 -c 'logs-pwr' -l 'esc01,iad01'                                                                                [13:33:25]
 
-UnFreeze_Index (Python Script) has started...
-
-The Script found the following indices to be opened:
-┏━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━┓
-┃ Cluster    ┃ Indice                                     ┃ Frozen_Status   ┃
-┣━━━━━━━━━━━━╋━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╋━━━━━━━━━━━━━━━━━┫
-┃ es1/9201   ┃ .ds-es1-logs-pas-main-stat-2023.04.25-0019 ┃ False           ┃
-┣━━━━━━━━━━━━╋━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╋━━━━━━━━━━━━━━━━━┫
-┃ es2/9201   ┃ .ds-es1-logs-pas-main-stat-2023.04.23-0011 ┃ False           ┃
-┗━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━━┛
-
-Would you like to open these Indices? [y/n]: yes
+[13:33:42] UNFREEZE Index Script (v1.1.1  04/25/2024)                                                                                       unfreeze_index.py:316
+           Query: DATE: [2024.03.26], COMPONENT: ['logs-pwr'], LOCATIONS ['esc01', 'iad01']                                                 unfreeze_index.py:316
+[13:33:57] Search Results: Cluster esc01 : 14 matches found.                                                                                unfreeze_index.py:416
+[13:34:04] Search Results: Cluster iad01 : 00 matches found.                                                                                unfreeze_index.py:416
 
 
-Action: Skipping Indice .ds-es1-logs-pas-main-stat-2023.04.25-000019, because not in Frozen Status
-Action: Skipping Indice .ds-es2-logs-pas-main-stat-2023.04.23-000011, because not in Frozen Status
+                                              Frozen Indices (action)                                              
+┏━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━┓
+┃ Cluster        ┃ Index                                                                      ┃ Frozen Status     ┃
+┡━━━━━━━━━━━━━━━━╇━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╇━━━━━━━━━━━━━━━━━━━┩
+│ esc01/9201     │ .ds-esc01-c01-logs-pwr-error_pwr_pasapi-2024.03.21-000018                  │ True              │
+└────────────────┴────────────────────────────────────────────────────────────────────────────┴───────────────────┘
+                                            Unfrozen Indices (noaction)                                            
+┏━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━┓
+┃ Cluster        ┃ Index                                                                      ┃ Frozen Status     ┃
+┡━━━━━━━━━━━━━━━━╇━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╇━━━━━━━━━━━━━━━━━━━┩
+│ esc01/9201     │ .ds-esc01-c01-logs-pwr-access_pwr_wsgapi-2024.03.22-000114                 │ False             │
+│ esc01/9201     │ .ds-esc01-c01-logs-pwr-access_pwr_api-2024.03.26-000136                    │ False             │
+│ esc01/9201     │ .ds-esc01-c01-logs-pwr-access_pwr_intapi-2024.03.23-000117                 │ False             │
+│ esc01/9201     │ .ds-esc01-c01-logs-pwr-access_pwr_pasapi-2024.03.24-000086                 │ False             │
+│ esc01/9201     │ .ds-esc01-c01-logs-pwr-access_pwr_vmt-2024.03.25-000086                    │ False             │
+│ esc01/9201     │ .ds-esc01-c01-logs-pwr-error_pwr_vmt-2024.03.21-000018                     │ False             │
+│ esc01/9201     │ .ds-esc01-c01-logs-pwr-error_pwr_api-2024.03.26-000025                     │ False             │
+│ esc01/9201     │ .ds-esc01-c01-logs-pwr-error_pwr_intapi-2024.03.26-000020                  │ False             │
+│ esc01/9201     │ .ds-esc01-c01-logs-pwr-gudproxy-2024.03.20-000019                          │ False             │
+│ esc01/9201     │ .ds-esc01-c01-logs-pwr-default-2024.03.20-000001                           │ False             │
+│ esc01/9201     │ .ds-esc01-c01-logs-pwr-nginx-rc-2024.03.20-000016                          │ False             │
+│ esc01/9201     │ .ds-esc01-c01-logs-pwr-error_pwr_wsgapi-2024.03.20-000019                  │ False             │
+└────────────────┴────────────────────────────────────────────────────────────────────────────┴───────────────────┘
+Would you like to open these Indices? [y/n]: y
+
+
+[13:34:08] Indice (unfrozen): .ds-esc01-c01-logs-pwr-error_pwr_pasapi-2024.03.21-000018                                                     unfreeze_index.py:225
 ```
 
-The script will ONLY unfreeze Indices that are in FROZEN, as you can see above it didn't unfreeze because the indices were already opened. 
+If there are no matches you will see something similar to this:
+```commandline
+└─[$] ./unfreeze_index.py -d 2024.03.26 -c 'blah' -l 'esc01'                                                                                          [13:20:16]
+
+[13:20:17] UNFREEZE Index Script (v1.1.1  04/25/2024)                                                                                       unfreeze_index.py:316
+[13:20:18] Query: DATE: [2024.03.26], COMPONENT: ['blah'], LOCATIONS ['esc01']                                                              unfreeze_index.py:316
+[13:20:27] Search Results: Cluster esc01 : 00 matches found.                                                                                        unfreeze_index.py:416
+
+
+╭────────────────────────────── No Results Found ──────────────────────────────╮
+│                 The Script found no frozen indices to open!                  │
+╰──────────────────────────────────────────────────────────────────────────────╯
+
+```
+
+The script will ONLY unfreeze Indices that are in FROZEN. You can see by the output above that it shows 2 tables, one for Frozen (action) indices, and one for Unfrozen (no-action).
 
 ## Dependencies:
-You will want to install the Python pip3 libraries listed in the requirements.txt.
-In order to do the fancy output i use `tabulate` python module for example.
+Please ensure you install all the Python3 modules needed by running.
+```
+pip3 install -r requirements.txt
+```
 
 The  servers definitions are inside file called: `unfreeze_servers.txt`
-This was seperated from main script so that servers can be easily updated without touching main script.
-
+This was separated from main script so that servers can be easily updated without touching main script.
--- unfreeze_index/unfreeze_index.py
+++ unfreeze_index/unfreeze_index.py
@@ -2,8 +2,9 @@
 '''
  Index Opener (DataStream) version.
  Written by Devin Acosta
- v 1.0.1
 '''
+VERSION = "1.1.1"
+DATE = "04/25/2024"
 
 import re
 import requests
@@ -11,17 +12,29 @@ import argparse, subprocess
 from elasticsearch import Elasticsearch, ConnectionTimeout
 from collections import defaultdict
 import datetime
-from tabulate import tabulate
+from rich import print
+from rich.console import Console
+from rich.layout import Layout
+from rich.table import Table
+from rich.panel import Panel
+from rich.columns import Columns
+from rich.progress import Progress, Spinner
+from rich.syntax import Syntax
+from rich.text import Text
+from rich import box
+from rich.box import Box
 import pickle
-
+import time
 
 
 def Merge(dict1, dict2):
     res = dict1 | dict2
     return res
 
+
 def returnDict():
     return {}
+
 
 def find_most_likely_file(filenames, date):
 
@@ -43,6 +56,7 @@ def find_most_likely_file(filenames, date):
             most_likely_file = filename
 
     return most_likely_file
+
 
 def find_closest_file(filenames, date):
     # Convert the date string to a datetime object
@@ -84,6 +98,7 @@ def uniquePatterns(filenames):
     ret_filenames = list(set(ret_filenames))
     return ret_filenames
 
+
 def getIndices(location,es_port):
 
     # Convert to upper case so that it matches Variable SJC01/IAD01/etc...
@@ -120,22 +135,22 @@ def getIndices(location,es_port):
     #print(location, es_indices)
     return es_indices
 
+
 '''
 Expects input to be a key=>value pair
 '''
-def processResults(indices,pattern):
-
+def processResults(indices, patterns):
     matching_keys = []
-    search_pattern = f".*{pattern}.*"
-    regex_pattern = re.compile(search_pattern)
+    regex_patterns = [re.compile(f".*{pattern}.*") for pattern in patterns]
 
     # Loop through indices and find matches
     for key in indices.keys():
-
-        if regex_pattern.match(key):
+        if any(regex_pattern.match(key) for regex_pattern in regex_patterns):
             matching_keys.append(key)
 
     return matching_keys
+
+
 '''
 Same as processResults but is simple list.
 '''
@@ -152,6 +167,7 @@ def matchingIndices(indices,pattern):
             matching_keys.append(key)
 
     return matching_keys
+
 
 '''
 Get the Frozen Status of an indice
@@ -176,6 +192,7 @@ def get_frozenStatus(data, indice):
     else:
         return None
 
+
 '''
 Function: Unfreeze single indice
 '''
@@ -192,7 +209,6 @@ def action_unFreezeIndice(host,port,indice):
 Function: Get matching indices and actually unfreeze them
 '''
 def unFreezeIndices(batched_final):
-    #print("UnFreeze, Batched_final", batched_final)
     for indice in batched_final:
         indice_data = batched_final[indice]
         indice_location = indice_data['location']
@@ -200,41 +216,67 @@ def unFreezeIndices(batched_final):
         indice_freeze = indice_data['frozen_status']
 
         if (indice_freeze == True):
-            print(f"Action: Unfreezing Indice {indice}")
-            ufs_response, ufs_data = action_unFreezeIndice(indice_location,indice_port,indice)
-            print(ufs_response,ufs_data)
-            if (ufs_response == 200):
-                print(f"Action: Completed, {indice} has been unfrozen.")
+
+            with console.status("Unfreezing Indices...") as status:
+                status.update(f"Action: Unfreezing Indice {indice}")
+                ufs_response, ufs_data = action_unFreezeIndice(indice_location,indice_port,indice)
+                #print(ufs_response,ufs_data)
+                if (ufs_response.status_code == 200):
+                    console.log(f"Indice (unfrozen): {indice}")
 
         elif (indice_freeze == False):
-            print(f"Action: Skipping Indice {indice}, because not in Frozen Status")
-
-        #print(indice, indice_freeze, indice_location, indice_port)
-
-
-'''
-Pretty Print Results to Screen and get acknowledgement to proceed
-'''
-def displayResults(tabdata,batched_final):
-
-    if len(tabdata) == 0:
-        print("The Script found [- No results -] to open!\n\n")
-        exit()
+            # Not going to log anything.
+            pass
 
 
-    print("\nThe Script found the following indices to be opened:")
-    print(tabulate(tabdata,tablefmt="heavy_grid",headers=["Cluster","Indice","Frozen_Status"]))
-
-    proceed = validateProceed()
-    if proceed == True:
-        '''
-        Proceed to unfreeze indices
-        '''
-        print("\n")
-        unFreezeIndices(batched_final)
+# Create tables
+def create_table(title, data):
+    table = Table(title=title, width=115)
+    table.add_column("Cluster")
+    table.add_column("Index")
+    table.add_column("Frozen Status")
+    if len(data) == 0:
+        table.add_row("N/A", "No results found", "N/A")
     else:
-        print("You have aborted operations!")
+        for index, row_data in data.items():
+            index_cluster = f"{row_data['location']}/{row_data['port']}"
+            index_frozen_status = f"{row_data['frozen_status']}"
+            table.add_row(index_cluster, index, index_frozen_status)
+    return table
+
+'''
+Display Results to Screen
+'''
+def displayResults(frozen_final, unfrozen_final, batched_final):
+
+    global console
+
+    print("\n")
+
+    # Create the Tables
+    frozen_table = create_table("Frozen Indices (action)", frozen_final)
+    unfrozen_table = create_table("Unfrozen Indices (noaction)", unfrozen_final)
+
+    console.print(frozen_table)
+    console.print(unfrozen_table)
+
+
+    # Determine if there are any indices to open or not
+    any_true = any(item['frozen_status'] for item in batched_final.values())
+    if any_true == False:
+        console.log(f"There are no FROZEN indices to open, exiting...\n\n")
         exit()
+    else:
+        proceed = validateProceed()
+        if proceed == True:
+            '''
+            Proceed to unfreeze indices
+            '''
+            print("\n")
+            unFreezeIndices(batched_final)
+        else:
+            print("You have aborted operations!")
+            exit()
 
 
 def tabData(data):
@@ -264,10 +306,33 @@ def validateProceed():
 
 
 '''
+Function to update the status bar with latest data.
+'''
+def status_bar_update(message):
+    global console
+
+    with console.status(f"[bold green]{message}") as status:
+        time.sleep(0.5)
+        console.log(message)
+
+
+def show_message_box(title, message, message_style="bold white", panel_style="white on blue"):
+
+    message = Text(f"{message}", style=message_style, justify="center")
+    panel = Panel(message, style=panel_style, title=title, border_style="bold white", width=80)
+    console.print("\n")
+    console.print(panel)
+    console.print("\n")
+
+'''
 Main Function starts here.
 '''
 if __name__ == "__main__":
 
+    # Start Rich Console...
+    console = Console()
+    print("")
+    
     '''
     Load Variables from unfreeze_servers.txt
     Please Ensure All hostnames are CAPITAL in the unfreeze_servers.txt
@@ -276,7 +341,7 @@ if __name__ == "__main__":
         variables = f.read()
         exec(variables, globals())
 
-
+    # Parser Stuff
     parser = argparse.ArgumentParser()
     # Locations(split by ,) where indexes need to be opened
     parser.add_argument("-l", "--locations", required=True)
@@ -287,60 +352,82 @@ if __name__ == "__main__":
     args = parser.parse_args()
     locations=args.locations.split(',')
     dt=args.date
-    comp=args.component
+    comp=args.component.split(',')
     indx2openList=[]
 
-    print("\nUnFreeze_Index (Python Script) has started...\n")
+    # Print to Console script starting... then progress bar.
+    status_bar_update(f"UNFREEZE Index Script [white](v{VERSION}  {DATE})[/white]")
+    status_bar_update(f"Query: DATE: [{dt}], COMPONENT: [bold green]{comp}[/bold green], LOCATIONS {locations}")
     batched_final = defaultdict(dict)
 
-    for location in locations:
+    time.sleep(1)
+    with console.status("Collecting data...") as status:
 
-        # Get Indices (from all ports for the Instance)
-        rc_indices_1 = getIndices(location,9201)
-        rc_indices_2 = getIndices(location,9202)
-        rc_indices_3 = getIndices(location,9203)
-        rc_indices_4 = getIndices(location,9200)
+        for location in locations:
+            status.update(f"Collecting data from cluster: {location}")
 
-        # Create new dictionary and append results of last step.
-        merged_indices = {}
-        merged_indices.update(rc_indices_1[location])
-        merged_indices.update(rc_indices_2[location])
-        merged_indices.update(rc_indices_3[location])
-        merged_indices.update(rc_indices_4[location])
+            # Get Indices (from all ports for the Instance)
+            rc_indices_1 = getIndices(location,9201)
+            rc_indices_2 = getIndices(location,9202)
+            rc_indices_3 = getIndices(location,9203)
+            rc_indices_4 = getIndices(location,9200)
+
+            # Create new dictionary and append results of last step.
+            merged_indices = {}
+            merged_indices.update(rc_indices_1[location])
+            merged_indices.update(rc_indices_2[location])
+            merged_indices.update(rc_indices_3[location])
+            merged_indices.update(rc_indices_4[location])
 
 
-        # Look for matches of indices
-        matching_indices = processResults(merged_indices,comp)
-        unique_indices_patterns = uniquePatterns(matching_indices)
+            # Look for matches of indices
+            matching_indices = processResults(merged_indices,comp)
+            unique_indices_patterns = uniquePatterns(matching_indices)
 
-        # Loop through each unique pattern and get that match
-        final_matching_indices = []
-        for match in unique_indices_patterns:
-            _matches = matchingIndices(matching_indices, match)
-            most_likely_match = find_closest_file(_matches, dt)
-            # Only Append if there is something to append
-            if most_likely_match != None:
-                final_matching_indices.append(most_likely_match)
+            # Loop through each unique pattern and get that match
+            final_matching_indices = []
+            for match in unique_indices_patterns:
+                _matches = matchingIndices(matching_indices, match)
+                most_likely_match = find_closest_file(_matches, dt)
+                # Only Append if there is something to append
+                if most_likely_match != None:
+                    final_matching_indices.append(most_likely_match)
 
-        '''
-        Create (batched_final) and append all indices so we can open those at the end.
-        '''
-        if (len(final_matching_indices) > 0 ):
-            for indices in final_matching_indices:
-                port = merged_indices[indices]['port']
-                batched_final[indices] = { "location": location, "port": port }
-        #unFreezeIndices(merged_indices, final_matching_indices)
+            '''
+            Create (batched_final) and append all indices so we can open those at the end.
+            '''
+            if (len(final_matching_indices) > 0 ):
+                for indices in final_matching_indices:
+                    port = merged_indices[indices]['port']
+                    batched_final[indices] = { "location": location, "port": port }
 
-        # Loop Through Batched_Final to get Frozen Status
-        for indice in batched_final:
-            _indice_frozen_status = get_frozenStatus(batched_final,indice)
-            if _indice_frozen_status == True:
-                batched_final[indice]['frozen_status'] = _indice_frozen_status
-            elif _indice_frozen_status == False:
-                batched_final[indice]['frozen_status'] = _indice_frozen_status
-            else:
-                batched_final[indice]['frozen_status'] = None
+            matches = len(final_matching_indices)
+
+            # Loop Through Batched_Final to get Frozen Status
+            for indice in batched_final:
+                _indice_frozen_status = get_frozenStatus(batched_final,indice)
+                if _indice_frozen_status == True:
+                    batched_final[indice]['frozen_status'] = _indice_frozen_status
+                elif _indice_frozen_status == False:
+                    batched_final[indice]['frozen_status'] = _indice_frozen_status
+                else:
+                    batched_final[indice]['frozen_status'] = None
+    
+            console.log(f"Search Results: Cluster {location} : {matches:02} matches found.")
+
+    # Break Dictionaries into what we need to action and not action on.
+    frozen_final = {}
+    unfrozen_final = {}
+
+    for indice, data in batched_final.items():
+        # Determine the target dictionary based on the 'frozen_status' value
+        target_dict = frozen_final if data['frozen_status'] else unfrozen_final
+        target_dict[indice] = data
 
     tabdata = tabData(batched_final)
-    displayResults(tabdata,batched_final)
+    # Exit if Nothing to Process
+    if len(tabdata) == 0:
+        show_message_box("No Results Found", "The Script found no frozen indices to open!")
+        exit()
+    displayResults(frozen_final, unfrozen_final,batched_final)
     print("\n")
